94601e0bda50393f3f12e9d2d8bb8a7e99a51fec
ofir-PC\ofir cohen <ofir7781@gmail.com>
2018-05-27 16:32:56 +0300

    chnge

--- CheckersWinUI/GameForm.cs
+++ CheckersWinUI/GameForm.cs
@@ -8,14 +8,6 @@ namespace CheckersWinUI
 {
     public class GameForm : Form
     {
-        private const int k_SmallBoardSize = 6;
-        private const int k_MediumBoardSize = 8;
-        private const int k_BigBoardSize = 10;
-        private const int k_LengthIncrease = 15;
-        private const int k_WidthIncrease = 10;
-        private const int k_InitialLengthIncrease = 200;
-        private const int k_InitialWidthIncrease = 100;
-
         private readonly LogicUnit r_LogicUnit;
         private readonly List<CheckersSoldier> r_CheckersBoard;
 
@@ -27,32 +19,18 @@ namespace CheckersWinUI
             this.Text = "Damka";
             this.ShowIcon = false;
             r_LogicUnit = new LogicUnit(i_BoardSize);
+            r_CheckersBoard = new List<CheckersSoldier>(i_BoardSize * i_BoardSize);
             createBoard();
             r_LogicUnit.CreatePlayerOne(i_Player1Name);
-            r_CheckersBoard = new List<CheckersSoldier>(i_BoardSize * i_BoardSize);
         }
 
         private void createBoard()
         {
             r_LogicUnit.CreateBoard();
-            
-            switch(r_CheckersBoard.Count)
-            {
-                case k_SmallBoardSize:
-                    BuildCheckersBoard(r_CheckersBoard.Count);
-                    break;
-                case k_MediumBoardSize:
-                    BuildCheckersBoard(r_CheckersBoard.Count);
-                    break;
-                case k_BigBoardSize:
-                    BuildCheckersBoard(r_CheckersBoard.Count);
-                    break;
-                default:
-                    break;
-            }
+            buildCheckersBoard(r_CheckersBoard.Count);
         }
 
-        private void BuildCheckersBoard(int i_BoardSize)
+        private void buildCheckersBoard(int i_BoardSize)
         {
             for(int row = 0; row < i_BoardSize; row++)
             {
@@ -73,11 +51,10 @@ namespace CheckersWinUI
                     else
                     {
                         r_CheckersBoard[row * i_BoardSize + col] = new CheckersSoldier(CheckersSoldier.eSoldierType.None, false);
-                    }
-
-                    
+                    }                 
                 }
             }
+            
 
 
         }
